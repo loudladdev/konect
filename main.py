6cf6d4599bb917db4d1b1303452ccb1c78921276
loudladdev <loudlad123@gmail.com>
2023-08-01 15:09:57 +0100

    major changes made to main and init

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from website import create_app
-
 app = create_app()
-
-if _name__ == '__main__':
+if __name__ == '__main__':
 	app.run(debug = True)
--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
-
-def create_app(app):
+@app.route('/')
+def home():
+	return '<h1>Welcome to home </h1>'
+def create_app():
 	app.config['SECRET_KEY'] = "jshiihhheii"
 	return app
